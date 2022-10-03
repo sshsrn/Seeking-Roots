f799d1fcc6535c2e76bd2842b173bf84b7f8fe7c
Srishty Mangutte <68679980+Srish-ty@users.noreply.github.com>
2022-10-03 12:21:00 +0530

    Create functional.js

---
+++ functional.js
@@ -0,0 +1,8 @@
+function toggle_visibility(){
+    var touch=document.getElementById('pswd');
+    if (x.type == "password") {
+     touch.type = "text";
+   } else {
+     touch.type = "password";
+   }
+ }
